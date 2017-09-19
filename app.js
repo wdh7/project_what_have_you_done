0910aed2392849cf2025ac7aa1cd9108d2c978d6
William Huynh <31399549+wdh7@users.noreply.github.com>
2017-09-18 22:49:40 -0700

    Create index route and view

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const index = require('./routes/index.js');
 
 
 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', index);
 
 
 
